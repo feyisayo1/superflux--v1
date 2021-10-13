286c6ece6260a332e4b559ad71f87107a88c2de3
feyisayo1 <45480719+feyisayo1@users.noreply.github.com>
2021-10-13 01:24:04 +0100

    files upload

---
+++ slider.js
@@ -0,0 +1,14 @@
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:3,
+    loop:true,
+    margin:10,
+    // nav:true,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true
+});
+
+
+

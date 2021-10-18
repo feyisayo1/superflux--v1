24ce459e16efee859c477f93cf0f644dc0c912a2
feyisayo1 <45480719+feyisayo1@users.noreply.github.com>
2021-10-18 22:45:42 +0100

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,42 @@
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:3,
+    loop:true,
+    margin:10,
+    // nav:true,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true
+});
+
+
+
+
+var owl = $('.owl-carousel1');
+owl.owlCarousel({
+    items:2,
+    loop:true,
+    margin:10,
+    // nav:true,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true
+});
+
+
+
+
+var owl = $('.owl-carousel2');
+owl.owlCarousel({
+    items:6,
+    loop:true,
+    margin:10,
+    // nav:true,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true
+});
+
+
+
